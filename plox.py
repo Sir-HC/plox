baa98f128f07afef00a9b6e624688c6db19a444e
Lynch <chris@taco.rehab>
2022-07-01 10:25:22 -0700

    Error handling 4.1.1/Classify Plox

--- plox.py
+++ plox.py
@@ -1,32 +1,46 @@
 import sys
 
-class Plox():
-    pass
+class Plox:
+    def __init__(self, args):
+        self.hadError = False
+        if len(args) > 2:
+            print("Usage: plox [script]")
+            quit()
+        elif len(args) == 2:
+            self.runFile(args[1])
+        else:
+            self.runPrompt()
 
-def runFile(path):
-    bytes = b''
-    with open(path, 'rb') as bf:
-        bytes = bf.readall()
-    run(bytes.decode(sys.getdefaultencoding()))
-    
-def runPrompt():
-    
-    while True:
-        print("> ", end= '')
-        line = input()
-        if line == "":
-            break
-        run(line)
+    def runFile(self, path):
+        bytes = b''
+        with open(path, 'rb') as bf:
+            bytes = bf.read()
+        print(f"Running with encoding: {sys.getdefaultencoding()}")
+        self.run(bytes.decode(sys.getdefaultencoding()))
+        if self.hadError:
+            quit()
+        
+    def runPrompt(self):
+        
+        while True:
+            print("> ", end= '')
+            line = input()
+            if line == "":
+                break
+            self.run(line)
+            self.hadError = False
 
-def run(string):
-    pass
+    def run(self, string):
+        print(string)
+
+    def error(self, line_no, msg):
+        report(line_no, "", msg)
+        
+    def report(self, line_no, where, msg):
+        print(f"[line {line_no}] Error {where}: {msg}", file=sys.stderr)
+        self.hadError = True
+    
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        print("Usage: plox [script]")
-        quit()
-    elif len(sys.argv) == 2:
-        runFile(sys.argv[1])
-    else:
-        runPrompt()
+    Plox(sys.argv)
 
